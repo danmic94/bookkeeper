22834d578e28e4bca66b54d10a24e1c8478a4033
danm94 <micdan1994@gmail.com>
2015-05-18 13:02:52 +0300

    First commmit with routes and rendered tiwgs: edit index new show.

---
+++ bookkeeper/src/Bookkeeper/ManagerBundle/Controller/BookController.php
@@ -0,0 +1,38 @@
+<?php
+
+
+ namespace Bookkeeper\ManagerBundle\Controller;
+
+ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+ use Symfony\Component\HttpFoundation\Request;
+
+ class BookController extends Controller {
+
+     public function indexAction(){
+         return $this->render('BookkeeperManagerBundle:Book:index.html.twig');
+     }
+
+     public function showAction($id){
+        return $this->render('BookkeeperManagerBundle:Book:show.html.twig');
+     }
+
+     public function newAction(){
+
+     }
+
+     public function createAction(Request $request){
+
+     }
+
+     public function editAction($id){
+         return $this->render('BookkeeperManagerBundle:Book:edit.html.twig');
+     }
+
+     public function updateAction(Request $request, $id){
+
+     }
+
+     public function deleteAction(Request $request, $id){
+
+     }
+ }
